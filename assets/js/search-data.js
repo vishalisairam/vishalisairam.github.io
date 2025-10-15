9225cfb965db17779ca4a9fb0967924337372e60
vishalisairam <67740260+vishalisairam@users.noreply.github.com>
2025-10-15 10:45:45 +0000

    Deploying to gh-pages from @ vishalisairam/vishalisairam.github.io@cd7de26247bf0270dd3636e171cf9ac649906a9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -455,11 +455,11 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-inspir ehep_id',
+        title: 'Inspir ehep_id',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("", "_blank");
         },
       },{
         id: 'social-rss',
@@ -474,13 +474,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
